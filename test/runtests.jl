33f23a9475a17034a7874fc5fe5f361aa63c6cf7
tgautam03 <tushariitr3@gmail.com>
2022-12-24 18:50:58 -0700

    Test for basic Tensor addition

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,16 @@
 using Test
+using jac
 
-@test true
+function test_Tensor()
+    arr1 = rand(5, 1)
+    arr2 = rand(5, 1)
+    arr_sum = arr1 + arr2
+    
+    T1 = Tensor(arr1)
+    T2 = Tensor(arr2)
+    T_sum = T1 + T2
+
+    cond = (T_sum == arr_sum)
+end
+
+@test test_Tensor()
