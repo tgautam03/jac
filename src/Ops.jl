05d73691330cc2768be848a8952b68d8350d5fd2
tgautam03 <tushariitr3@gmail.com>
2022-12-26 15:38:38 -0700

    Added add and mul ops

--- src/Ops.jl
+++ src/Ops.jl
@@ -4,5 +4,10 @@ include("Tensor.jl")
 
 # Addition Operation on Tensor types
 function Base.:+(T1::Tensor, T2::Tensor)
-    return T1.val + T2.val
+    return Tensor(T1.val + T2.val, [T1, T2], [ones(size(T1)), ones(size(T2))])
+end
+
+# Multiplication Operation on Tensor types
+function Base.:*(T1::Tensor, T2::Tensor)
+    return Tensor(T1.val * T2.val, [T1, T2], [T2.val, T1.val])
 end
