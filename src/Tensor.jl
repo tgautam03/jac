44c207d5fd0acd833c3180e3bf57c3f8c2180292
tgautam03 <tushariitr3@gmail.com>
2022-12-26 15:38:56 -0700

    Modified the structure of Tensor

--- src/Tensor.jl
+++ src/Tensor.jl
@@ -1,8 +1,16 @@
 mutable struct Tensor
-    val::Array # Value of Tensor
+    val::Array          # Value of Tensor
+    creators::Vector    # Parents of this Tensor
+    local_grads::Vector # Local Gradient wrt creators
 
-    # Constructor
+    # Constructor 1
     function Tensor(val::Array)
-        new(val)
+        new(val, [], [])
+    end
+
+    # Constructor 2
+    function Tensor(val::Array, creators::Vector{Tensor}, local_grads::Vector{<:Array})
+        @assert (size(creators)[1] == size(local_grads)[1]) # Make sure local_grads for each creator is provided
+        new(val, creators, local_grads)
     end
 end
