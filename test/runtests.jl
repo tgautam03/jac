224db26fba341a590f4562f3aa4b7c600f30c328
tgautam03 <tushariitr3@gmail.com>
2022-12-26 15:40:14 -0700

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,27 @@ function test_Tensor()
     T2 = Tensor(arr2)
     T_sum = T1 + T2
 
-    cond = (T_sum == arr_sum)
+    return (T_sum.val == arr_sum)
+end
+
+function test_backward()
+    A = Tensor([5][:,:])
+    B = Tensor([3][:,:])
+    
+    C = A + B
+    D = B * C
+
+    dA = grad(D)[A]
+    dB = grad(D)[B]
+    dC = grad(D)[C]
+
+    cond1 = (D.val == [24][:,:])
+    cond2 = (dA == [3][:,:])
+    cond3 = (dB == [11][:,:])
+    cond4 = (dC == [3][:,:])
+
+    return (cond1 == true && cond2 == true && cond3 == true && cond4 == true)
 end
 
 @test test_Tensor()
+@test test_backward()
