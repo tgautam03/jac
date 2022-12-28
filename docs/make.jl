d516eeb6b11e3eade6f33a20499cf43df069b89e
tgautam03 <tushariitr3@gmail.com>
2022-12-27 19:47:02 -0700

    Updated docs

--- docs/make.jl
+++ docs/make.jl
@@ -9,9 +9,11 @@ makedocs(
         "Home" => "index.md",
         "Development Blogs" => Any[
             "Development/AD_0.1.md",
+            "Development/AD_0.5.md",
         ],
         "Examples" => Any[
             "Examples/Intro_AD.md",
+            "Examples/Basic_NN.md",
         ],
     ],
 )
