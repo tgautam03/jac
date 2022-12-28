674b6e85751d905a2a4b4563a6b485e1fda4c357
tgautam03 <tushariitr3@gmail.com>
2022-12-27 19:50:58 -0700

    Updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,42 +1,6 @@
 using Test
 using jac
 
-# function test_grad()
-    # X = Tensor([0.4963 0.7682 0.0885;
-    # 0.1320 0.3074 0.6341;
-    # 0.4901 0.8964 0.4556;
-    # 0.6323 0.3489 0.4017;
-    # 0.0223 0.1689 0.2939])
-
-    # W = Tensor([0.5185 0.6977;
-    #     0.8000 0.1610;
-    #     0.2823 0.6816])
-
-    # b = Tensor([0.9152 0.3971])   
-
-    # Y = Tensor(ones(5,2))
-
-    # Z = X * W + b
-
-    # A = sum((Z - Y)^2)
-
-    # L = A/size(X)[1]
-
-    # grads = jac.grad(L)
-
-#     return (grads, grads[A], grads[Z], grads[Y], grads[b], grads[W], grads[X])
-# end
-
-# (grads, A, Z, Y, b, W, X) = test_grad()
-
-
-# A
-# Z
-# Y
-# b
-# W
-# X
-
 function test_Tensor()
     arr1 = rand(5, 1)
     arr2 = rand(5, 1)
@@ -55,19 +19,19 @@ function test_backward()
     
     C = A * B
     D = sum(C)
-    println("D: \n", D)
-    println("")
+    # println("D: \n", D)
+    # println("")
 
     dA = jac.grad(D)[A]
     dB = jac.grad(D)[B]
     dC = jac.grad(D)[C]
 
-    println("dC: \n", dC)
-    println("")
-    println("dB: \n", dB)
-    println("")
-    println("dA: \n", dA)
-    println("")
+    # println("dC: \n", dC)
+    # println("")
+    # println("dB: \n", dB)
+    # println("")
+    # println("dA: \n", dA)
+    # println("")
     # cond1 = (D.val == [24][:,:])
     # cond2 = (dA == [3][:,:])
     # cond3 = (dB == [11][:,:])
