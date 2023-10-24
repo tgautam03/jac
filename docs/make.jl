bf0812987821eac35a808edff09766596a428d63
tgautam03 <tushariitr3@gmail.com>
2023-10-24 17:58:35 -0600

    Added docs and tests

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter
+using jac
+
+makedocs(
+    sitename = "jac",
+    format = Documenter.HTML(),
+    modules = [jac],
+    pages = [
+        "Home" => "index.md",
+    ],
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
