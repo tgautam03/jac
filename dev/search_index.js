6dd618d18e8b9049837ed14ab5ec1114dbf60fc8
Documenter.jl <documenter@juliadocs.github.io>
2023-10-25 00:14:42 +0000

    build based on e9f25de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#jac.jl","page":"Home","title":"jac.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for jac.jl","category":"page"}]
+}
